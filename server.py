078a544c1cf7f61edbc214705c6362528e3f575d
Bryan Ortega <fbryanortega@gmail.com>
2023-08-19 15:47:02 -0500

    Added flask routes for authentication and demo pages

--- server.py
+++ server.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 def display_home():
     return render_template("homepage.html")
 
+@app.route('/login')
+def display_login():
+    return render_template("auth.html")
 
+@app.route('/demo')
+def display_demo():
+    return render_template("demo.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
