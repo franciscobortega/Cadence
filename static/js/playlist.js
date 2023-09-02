95704f769a8b126f28a58e3e52889b2639291e1f
Bryan Ortega <fbryanortega@gmail.com>
2023-09-02 09:23:23 -0500

    Added function that splits route into segments

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -28,13 +28,18 @@ const tempoMappings = {
 const targetIntensity = "energetic";
 const { min: minTempo, max: maxTempo } = tempoMappings[targetIntensity]; // will be passed along to Get Recommendations endpoint
 
-console.log(minTempo);
-console.log(maxTempo);
+// console.log(minTempo);
+// console.log(maxTempo);
 
 // --------------- SPOTIFY --------------- //
 
 // 1. Fetch from Get Recommendations endpoint
-// TODO: fetch from Get Recommendations endpoint
+// TODO: fetch from Get Recommendations endpoint: up to 5 genres, popularity 0-100, min/max tempo
+let testGenres = "pop,rock,hip-hop";
+let testPopularity = 60;
+let testMinTempo = minTempo;
+let testMaxTempo = maxTempo;
+
 let playlistRecommendationsSampleResponse = [
   {
     album: {
@@ -296,14 +301,14 @@ let playlistRecommendationsSampleResponse = [
 let audioAnaylysisQueryString = "";
 
 playlistRecommendationsSampleResponse.forEach((track) => {
-  console.log(track.name);
-  console.log(track.artists[0].name);
-  console.log(track.id);
+  //   console.log(track.name);
+  //   console.log(track.artists[0].name);
+  //   console.log(track.id);
 
   audioAnaylysisQueryString += track.id + ",";
 });
 
-console.log(audioAnaylysisQueryString);
+// console.log(audioAnaylysisQueryString);
 
 // 3. Fetch from Get Tracks' Audio Features endpoint
 // TODO: fetch from Get Tracks' Audio Features endpoint
@@ -374,3 +379,38 @@ let audioAnalysisSampleResponse = [
 ];
 
 // 4.
+
+function splitRouteIntoSegments(elevationData, elevationThreshold) {
+  const segments = [];
+  let currentSegment = [elevationData[0]];
+
+  // First segment starts with first point, we loop through the rest
+  for (let i = 1; i < elevationData.length; i++) {
+    const elevationChange = elevationData[i] - elevationData[i - 1];
+
+    // If current elevationChange is greater than threshold start a new segment otherwise add point to the current segment
+    if (Math.abs(elevationChange) >= elevationThreshold) {
+      segments.push(currentSegment);
+      currentSegment = [elevationData[i]];
+    } else {
+      currentSegment.push(elevationData[i]);
+    }
+  }
+
+  // Add the last segment
+  segments.push(currentSegment);
+
+  return segments;
+}
+
+let testElevationData = [
+  225.53, 224.72, 224.11, 223.65, 223.35, 223.46, 225.26, 226.33, 226.46,
+  226.93, 227.06, 227.19, 227.22, 227.23, 227.25, 227.27, 227.34, 227.36, 227.6,
+  227.68, 227.72, 227.76, 227.95, 228.03, 228.06, 228.1, 227.14, 227.2, 227.3,
+  227.46, 228.21, 228.29, 228.53, 228.81, 229.1, 229.6, 229.74, 229.83, 229.91,
+  230.0, 230.78, 230.86, 230.95, 231.05, 231.21, 231.35, 231.42, 232.36, 233.48,
+  233.54, 232.32, 230.78, 230.67, 230.24, 230.65, 230.16, 230.07, 230.02,
+  229.98, 228.15, 226.32, 226.15, 225.96, 225.64,
+];
+
+console.log(splitRouteIntoSegments(testElevationData, 1.3));
