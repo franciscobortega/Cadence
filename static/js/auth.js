732f5d0dc131f356a6f1cb34aca798a24d90e64e
Bryan Ortega <fbryanortega@gmail.com>
2023-10-10 10:40:17 -0500

    Moved inline script to external file

--- static/js/auth.js
+++ static/js/auth.js
@@ -23,3 +23,76 @@ function redirectToAuthCodeFlow(clientId) {
 
   document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
+
+// --------------- Logic for handling user route updates  --------------- //
+
+const userRoutes = document.querySelectorAll(".route-card");
+userRoutes.forEach((route) => {
+  const routeForm = route.querySelector(".route-card-info");
+  const deleteButton = route.querySelector(".route-card-delete-btn");
+
+  routeForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const routeId = route.getAttribute("data-route-id");
+
+    const formData = new FormData(routeForm);
+    formData.append("route_id", routeId);
+    console.log(formData);
+
+    try {
+      const response = await fetch("/update-route", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+          console.log("Updating route!");
+
+          // Call function to update elements
+          const titleElement = document.querySelector(".route-card-title");
+          const descriptionElement = document.querySelector(
+            ".route-card-description"
+          );
+
+          const newTitle = formData.get("route-title");
+          const newDescription = formData.get("route-description");
+
+          titleElement.textContent = newTitle;
+          descriptionElement.textContent = newDescription;
+        }
+      } else {
+        console.error("Failed to update route.");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  deleteButton?.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const routeId = route.getAttribute("data-route-id");
+    const formData = new FormData(routeForm);
+    formData.append("route_id", routeId);
+
+    try {
+      const response = await fetch(deleteButton.formAction, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+          route.remove();
+        }
+      } else {
+        console.error("Failed to delete route.");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+});
